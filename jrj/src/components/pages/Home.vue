3d6975e3b17e706ace8f04c34d5de06525f5fed3
liyuanchuan <874057657@qq.com>
2018-11-15 17:09:55 +0800

    环境配置

---
+++ jrj/src/components/pages/Home.vue
@@ -0,0 +1,25 @@
+<template>
+<div class="home">
+	{{msg}}
+</div>
+</template>
+
+<script>
+	export default{
+		name:'Home',
+		components:{
+			
+		},
+		data(){
+			return {
+				msg:'  我是首页'
+			}
+		}
+	}
+</script>
+<style lang="less" scoped>
+@import url("../../styles/main.less");
+.home{
+	
+}
+</style>
